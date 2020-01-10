ed6391775dbc00dd05bed9a46fbbde5328472d73
HaidarTaqy <harak.gaming36@gmail.com>
2020-01-10 10:13:25 +0700

    Test Tiga

---
+++ Sprint-1/Topik-2/latihan-3.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 5;
+$b = 8;
+$c = $a*$b;
+echo "Panjang Persegi panjang : ". $a;
+echo "<br>";
+echo "Lebar Persegi panjang : ". $b;
+echo "<br>";
+echo "Luas Persegi tersebut adalah :" . $c;
+?>
+
---
+++ Sprint-1/Topik-2/latihan-4.php
@@ -0,0 +1,13 @@
+<?php
+$a = 9;
+$b = 3.14;
+$c = $b*$a*$a/4;
+$d = $b*$a;
+echo "Diameter lingkaran : ". $a;
+echo "<br>";
+echo "π lingkaran :". $b;
+echo "<br>";
+echo "Luas lingkaran tersebut adalah :". $c;
+echo "<br>";
+echo "Keliling lingkaran tersebut adalah :". $d; 
+?>
