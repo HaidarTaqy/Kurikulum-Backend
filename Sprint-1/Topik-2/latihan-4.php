04ebd25c74d7d11ce96f7f2c891f851d8975d80a
HaidarTaqy <harak.gaming36@gmail.com>
2020-01-17 09:49:35 +0700

    Test Empat

--- Sprint-1/Topik-2/latihan-4.php
+++ Sprint-1/Topik-2/latihan-4.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>latihan-4</title>
+</head>
+<body>
+ 
+</body>
+</html>
 <?php
 $a = 9;
 $b = 3.14;
---
+++ Sprint-1/Topik-3/Latihan-2.php
@@ -0,0 +1,10 @@
+<?php
+for ($n = 9; $n > 0; $n--)
+{
+  if ($n % 2 == 0 || $n == 9)
+  {
+  continue;
+  }
+echo $n;
+echo " ";
+}
